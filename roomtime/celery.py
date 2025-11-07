f1acd4a76c50686c445550d13ca298c0076e69e1
AbekD <adancho2009@gmail.com>
2025-11-07 21:17:10 +0500

    Add indication of scalability

--- roomtime/celery.py
+++ roomtime/celery.py
@@ -1,10 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-import environ
-
-env = environ.Env()
-environ.Env.read_env(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roomtime.settings")
 
@@ -12,6 +8,27 @@ app = Celery("roomtime")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
-@app.task(bind=True)
-def debug_task(self):
-    print(f"Request: {self.request!r}")
+app.conf.task_routes = {
+"bookings.tasks.*": {"queue": "bookings"},
+"notifications.tasks.*": {"queue": "notifications"},
+}
+app.conf.broker_connection_retry_on_startup = True
+app.conf.task_default_retry_delay = 10
+app.conf.task_annotations = {
+    '*': {'max_retries': 3},
+}
+app.conf.result_backend_transport_options = {
+    'visibility_timeout': 3600,
+    "socket_timeout": 10,
+}
+
+app.conf.broker_transport_options = {
+    "max_retries": 5,
+    "interval_start": 0,
+    "interval_step": 2,
+    "interval_max": 10,
+}
+
+@app.task(name="core.health_check")
+def health_check():
+    return "Celery OK"
